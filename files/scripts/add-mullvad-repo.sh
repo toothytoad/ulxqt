ddc2b965395280e03128aff1a744c2f99c611316
toothytoad <toothytoad@angryteeth.net>
2024-08-03 17:23:34 -0700

    fix mullvad

--- files/scripts/add-mullvad-repo.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -oue pipefail
-
-curl -SsL https://repository.mullvad.net/rpm/stable/mullvad.repo | pkexec tee /etc/yum.repos.d/mullvad.repo
