5c55bcd17f0ae195ac6e53dc2b84a354fc03dfd4
toothytoad <toothytoad@angryteeth.net>
2024-08-07 19:58:24 -0700

    disable podman script

--- files/scripts/setpodmantrust.sh
+++ files/scripts/setpodmantrust.sh
@@ -3,7 +3,7 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-podman image trust set -t accept docker.io/ollama/ollama
-podman image trust set -t accept ghcr.io/open-webui/open-webui
+#podman image trust set -t accept docker.io/ollama/ollama
+#podman image trust set -t accept ghcr.io/open-webui/open-webui
 
 
