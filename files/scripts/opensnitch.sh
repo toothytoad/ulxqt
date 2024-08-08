88f9dda0a488ee524b81c867a8e593013c5294a0
toothytoad <toothytoad@angryteeth.net>
2024-08-07 19:27:28 -0700

    fix scripts? and  services

--- files/scripts/opensnitch.sh
+++ files/scripts/opensnitch.sh
@@ -22,4 +22,4 @@ rpm --import gustavo_iniguez_goia.asc
 
 dnf install -y opensnitch-1.6.6-1.x86_64.rpm opensnitch-ui-1.6.6-1.noarch.rpm
 
-systemctl enable opensnitch.service
+#systemctl enable opensnitch.service
