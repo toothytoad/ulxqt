5be5a20c80c89c0cec1c851bfa8de0ddb0996188
toothytoad <toothytoad@angryteeth.net>
2024-08-07 19:55:51 -0700

    typo

--- files/scripts/enablecups.sh
+++ files/scripts/enablecups.sh
@@ -3,7 +3,7 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-echo "Disabling the print service"
+echo "Enabling the print service"
 systemctl unmask cups
 systemctl enable cups
 
