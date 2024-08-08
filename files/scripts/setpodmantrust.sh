e2d779782c2afaba497a49413efb252c70de6965
toothytoad <toothytoad@angryteeth.net>
2024-08-07 18:05:35 -0700

    set podman trust

---
+++ files/scripts/setpodmantrust.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+podman image trust set -t accept docker.io/ollama/ollama
+podman image trust set -t accept ghcr.io/open-webui/open-webui
+
+
