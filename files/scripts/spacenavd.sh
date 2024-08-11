801be99378d5ee4447a9f96b15985d419b50867c
toothytoad <toothytoad@angryteeth.net>
2024-08-11 01:55:41 -0700

    disable remove build depends for now

--- files/scripts/spacenavd.sh
+++ files/scripts/spacenavd.sh
@@ -19,18 +19,10 @@ set -oue pipefail
 #  If you wish to install somewhere else, you may pass --prefix=/whatever to the configure script
 #
 #  For systems running systemd, there is a spacenavd.service file under contrib/systemd. Follow your system documentation for how to use it.
-
 #   libX11 libXi libXtst
 # musl-gcc
 
-#cd /tmp
-
- #     "browser_download_url": "https://github.com/FreeSpacenav/spacenavd/releases/download/v1.3/spacenavd-1.3.tar.gz"
-
-
-curl -L 'https://api.github.com/repos/FreeSpacenav/spacenavd/releases/latest' | \
-  awk -F \" -v RS="," '/tarball/ {print $(NF-1)}'
-  
+# https://stackoverflow.com/a/70903031  
 URL=$(wget https://api.github.com/repos/FreeSpacenav/spacenavd/releases/latest -O - | awk -F \" -v RS="," '/browser_download_url/ {print $(NF-1)}'); wget $URL -O $(basename "$URL")
 
 tar -xzvf $(basename "$URL") && cd $(basename -s '.tar.gz' $(basename "$URL"))
@@ -39,5 +31,5 @@ dnf install -y gcc make libX11-devel libXi-devel libXtst-devel
 
 bash configure && make && make install
 
-dnf uninstall -y gcc make libX11-devel libXi-devel libXtst-devel
+#dnf uninstall -y gcc make libX11-devel libXi-devel libXtst-devel
 
