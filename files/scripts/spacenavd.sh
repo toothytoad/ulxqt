afa4c323ab0cd0d41b8129133ce0d45011e0d25e
toothytoad <toothytoad@angryteeth.net>
2024-08-10 23:21:04 -0700

    fix download spacenavd

--- files/scripts/spacenavd.sh
+++ files/scripts/spacenavd.sh
@@ -20,19 +20,23 @@ set -oue pipefail
 #
 #  For systems running systemd, there is a spacenavd.service file under contrib/systemd. Follow your system documentation for how to use it.
 
-#  libX11 libXi libXtst 
+# libX11-devel libXi-devel libXtst-devel
+  
 # musl-gcc
 
 #cd /tmp
 
-#https://github.com/FreeSpacenav/spacenavd/releases/download/v1.3/spacenavd-1.3.tar.gz
-curl -s https://api.github.com/repos/FreeSpacenav/spacenavd/releases/latest \
-    | grep -wo "https.*spacenavd.tar.gz" | wget -O spacenavd.tar.gz -cqi -
+ #     "browser_download_url": "https://github.com/FreeSpacenav/spacenavd/releases/download/v1.3/spacenavd-1.3.tar.gz"
+
+
+curl -L 'https://api.github.com/repos/FreeSpacenav/spacenavd/releases/latest' | \
+  awk -F \" -v RS="," '/tarball/ {print $(NF-1)}'
+  
+URL=$(wget https://api.github.com/repos/FreeSpacenav/spacenavd/releases/latest -O - | awk -F \" -v RS="," '/browser_download_url/ {print $(NF-1)}'); wget $URL -O spacenavd.tar.gz
 
 tar -xzvf spacenavd.tar.gz && cd spacenavd
 
-dnf install -y gcc make libX11-devel libXi-devel libXtst-devel
-
+dnf install -y gcc make libX11 libXi libXtst
 ./configure && make && make install --prefix="/usr/local"
 
 #mkdir -p /etc/systemd/system/
