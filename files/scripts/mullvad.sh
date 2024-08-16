a8e6f23aa022f01db3c16b5d53c296b79b3d95ed
toothytoad <toothytoad@angryteeth.net>
2024-08-16 16:23:53 -0700

    fix mullvad

--- files/scripts/mullvad.sh
+++ files/scripts/mullvad.sh
@@ -6,7 +6,13 @@ set -oue pipefail
 
 # https://github.com/mullvad/mullvadvpn-app/issues/1570
 
-mkdir -p "/usr/lib/opt/Mullvad VPN/resources/mullvad-setup"
-ln -s '/usr/lib/opt/Mullvad VPN' '/opt/Mullvad VPN'
+mkdir -p "/usr/lib/opt/Mullvad VPN"
+
+optLink=/opt
+if [ ! -L "$optLink" ]; then 
+  ln -s /usr/lib/opt /opt
+fi
+
+#ln -s '/usr/lib/opt/Mullvad VPN' '/opt/Mullvad VPN'
 # semanage fcontext -a -t usr_t '/usr/lib/opt/Mullvad.VPN(/.*)?'
 
