ce0af1165a6d7441343f3583c3a6b2c4bd6d0b36
toothytoad <toothytoad@angryteeth.net>
2024-08-24 10:51:29 -0700

    fix build

--- files/scripts/spacenavd.sh
+++ files/scripts/spacenavd.sh
@@ -26,6 +26,6 @@ set -oue pipefail
 URL=$(wget https://api.github.com/repos/FreeSpacenav/spacenavd/releases/latest -O - | awk -F \" -v RS="," '/browser_download_url/ {print $(NF-1)}'); wget $URL -O $(basename "$URL") \
   && tar -xzvf $(basename "$URL") && rm -rf $(basename "$URL") && cd $(basename -s '.tar.gz' $(basename "$URL")) \
   && dnf install -y gcc make libX11-devel libXi-devel libXtst-devel \
-  && bash configure && make && make install \
+  && bash configure && make && make install #S\ 
 #  && dnf uninstall -y gcc make libX11-devel libXi-devel libXtst-devel \
-  && rm -rf $(basename -s '.tar.gz' $(basename "$URL"))
+#  && rm -rf $(basename -s '.tar.gz' $(basename "$URL"))
