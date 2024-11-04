a4396be05b3a4eaa82af1cb95c25110d2b583292
toothytoad <toothytoad@angryteeth.net>
2024-11-03 20:40:24 -0800

    fix build error

---
+++ files/scripts/installmissingpkgs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+curl -Lo /etc/yum.repos.d/_copr_atim_starship https://copr.fedorainfracloud.org/coprs/atim/starship/repo/fedora-"${OS_VERSION}"/atim-starship-fedora-"${OS_VERSION}".repo
+curl -Lo /etc/yum.repos.d/_copr_atim_lazygit https://copr.fedorainfracloud.org/coprs/atim/lazygit/repo/fedora-"${OS_VERSION}"/atim-lazygit-fedora-"${OS_VERSION}".repo
+curl -Lo /etc/yum.repos.d/_copr_dschubert_xiccd https://copr.fedorainfracloud.org/coprs/dschubert/xiccd/repo/fedora-"${OS_VERSION}"/dschubert-xiccd-fedora-"${OS_VERSION}".repo
+
+rpm-ostree install starship lazygit xiccd
+rm /etc/yum.repos.d/_copr_atim_starship.repo /etc/yum.repos.d/_copr_atim_lazygit.repo /etc/yum.repos.d/_copr_dschubert_xiccd.repo
